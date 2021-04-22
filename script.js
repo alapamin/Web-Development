e28070fc0d4fbe6ae2d6f2c3bf01f42482b45e37
alapamin <73265927+alapamin@users.noreply.github.com>
2021-04-22 01:30:04 -0400

    additions to list function & looks
    changed from solely js to jquery to make it more efficient, added strikethrough, added double click deletion, adjusted css file for better looks, still a WIP

---
+++ script.js
@@ -0,0 +1,54 @@
+$(document).ready(
+
+
+    function(){
+        $('#add').click(function(){
+            var toAdd = $('#task').val();
+            if(toAdd !== ''){
+                $('ul').append(  '<li>'  + toAdd +   '</li>'  );
+                $('task').val('');
+            }
+        }); 
+
+        $(document).on('click','li',function(){
+            $(this).toggleClass('cross');
+        });
+
+        $(document).on('dblclick','li',function(){
+            $(this).remove();
+        });
+
+    }
+
+
+);
+
+/*
+var addButton = document.getElementById("add");
+var input = document.getElementById("task");
+var toDoList = document.getElementById('to-do');
+
+addButton.addEventListener("click", addItem);
+
+function addItem(){
+    //li element
+    if(input.value !== ""){ //prevents from creation of ugly blank spaces
+        var listt = document.createElement("li");
+        //put the input as the text of the element
+        listt.innerHTML = input.value;
+        //add li to the list
+        toDoList.appendChild(listt);
+    
+        listt.addEventListener("click",function(){
+            listt.classList.toggle("cross")
+        });
+    
+        listt.addEventListener("dblclick",function(){
+            this.remove();
+        });
+    }
+    
+
+    input.value = "";
+}
+*/
