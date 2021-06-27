bbe2db675eb001095681464055161d718281c017
alapamin <73265927+alapamin@users.noreply.github.com>
2021-06-27 13:20:15 -0400

    Utilizing prebuilt camera control styles
    Specifically orbital controls. Had to provide the dom element this would occur in (the canvas), as well as the camera of choice.

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 
 /**
@@ -57,6 +58,10 @@ camera.position.z = 2
 camera.lookAt(mesh.position)
 scene.add(camera)
 
+//Controls
+const controls = new OrbitControls(camera, canvas) //canvas is the dom element
+controls.enableDamping = true
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
@@ -72,10 +77,16 @@ const tick = () =>
 
     // Update objects
     //mesh.rotation.y = elapsedTime;
-    camera.position.x = cursor.x
+    // camera.position.x = Math.sin(cursor.x * Math.PI*2) * 3 
+    // camera.position.z = Math.cos(cursor.x * Math.PI*2) * 3
+    // camera.position.y = cursor.y * 5
+    // camera.lookAt(mesh.position)
+
+    //Update controls
+    controls.update() //have to update on every frame
 
     // Render
-    renderer.render(scene, camera)
+    renderer.render(scene, camera)  
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
